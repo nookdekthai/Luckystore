bcf80103c721b32c0a8cc924ca22ce1dc76e828c
21 Tech Enterprise <21techenterprise@192.168.2.33>
2022-09-21 21:26:10 +0700

    first commit

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -3,6 +3,7 @@ import Head from 'next/head';
 
 import Navbar from './Navbar';
 import Footer from './Footer';
+import { Facebook1 } from './Facebook1';
 
 const Layout = ({ children }) => {
   return (
@@ -17,6 +18,10 @@ const Layout = ({ children }) => {
 <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"/>
 <meta name="msapplication-TileColor" content="#da532c"/>
 <meta name="theme-color" content="#ffffff"/>
+
+
+
+
       </Head>
       <header>
         <Navbar />
@@ -25,9 +30,12 @@ const Layout = ({ children }) => {
         {children}
       </main>
       <footer>
+        <Facebook1/>
         <Footer />
       </footer>
     </div>
+    
+    
   )
 }
 
